8b96edfebd3b7cd8b35a9149b34f9f253e9d8142
mahimfarhad <mahimfarhad.code@gmai.com>
2024-08-09 12:52:20 +0600

    Updating sidebar

--- components/layouts/Aside.jsx
+++ components/layouts/Aside.jsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import {
+  useState,
+  useMemo
+} from "react";
 
 import {
   m,
@@ -54,12 +57,11 @@ const contactItems = [{
   iconName: "Pin"
 }];
 
-function Info({ open, setOpen }) {
+function Info({ isOpen, setIsOpen }) {
   return (
     <Box
       className={clsx(
-        "flex",
-        "xl:flex-col",
+        "flex xl:flex-col",
         "gap-4 sm:gap-8",
         "items-center xl:items-start",
         "xl:h-full",
@@ -70,8 +72,7 @@ function Info({ open, setOpen }) {
           "max-xl:flex-shrink-0",
           "w-20 sm:w-24 xl:w-full",
           "h-20 sm:h-24 xl:h-full",
-          "overflow-hidden",
-          "dark:bg-white/10",
+          "dark:bg-[var(--jet)]",
           "bg-[url('https://i.pinimg.com/originals/f0/19/0e/f0190e4bac7a39672da2439f48d1e0f2.jpg')]",
           "bg-no-repeat bg-bottom bg-cover",
           "rounded-xl"
@@ -102,7 +103,7 @@ function Info({ open, setOpen }) {
             "py-0.5 px-2.5",
             "font-mono text-xs leading-[21px]",
             "font-medium uppercase",
-            "text-primary bg-white/10",
+            "text-primary bg-[var(--jet)]",
             "rounded-md"
           )}
         >
@@ -112,22 +113,18 @@ function Info({ open, setOpen }) {
 
       <Button
         className={clsx(
-          "absolute",
-          "top-0 right-0",
+          "absolute top-0 right-0",
           "xl:hidden",
-          "w-10 sm:w-auto",
-          "h-10",
-          "max-sm:p-2.5",
+          "w-10 sm:w-auto h-10 max-sm:p-2.5",
           "text-xs",
           "border-0 rounded-none rounded-bl-xl",
-          "service-item"
+          "gradient-bg"
         )}
-        onClick={() => setOpen(!open)}
+        onClick={() => setIsOpen(!isOpen)}
       >
-        <span className="hidden sm:block">{open ? "Hide" : "Show"} Contacts</span>
+        <span className="hidden sm:block">Contacts</span>
         <Icon
           name="ChevronDown"
-          className="block sm:hidden"
         />
       </Button>
     </Box>
@@ -148,8 +145,9 @@ function Contacts() {
           key={contactItem?.id}
           className={clsx(
             "flex flex-nowrap",
-            "gap-x-4 items-center",
-            "sm:w-1/2 xl:w-full",
+            "gap-x-4",
+            "items-center",
+            "sm:w-1/2 xl:w-full"
           )}
         >
           <div
@@ -158,7 +156,7 @@ function Contacts() {
               "flex items-center justify-center",
               "w-12 h-12 p-3",
               "rounded-xl",
-              "service-item",
+              "gradient-bg "
             )}
           >
             <Icon
@@ -176,7 +174,7 @@ function Contacts() {
             <Typography
               className={clsx(
                 "text-xs uppercase",
-                "text-white/50"
+                "text-gray-400"
               )}
             >
               {contactItem?.label}
@@ -186,7 +184,8 @@ function Contacts() {
               href={contactItem?.link['path']}
               className={clsx(
                 "overflow-hidden",
-                "text-sm text-ellipsis whitespace-nowrap",
+                "whitespace-nowrap",
+                "text-sm text-ellipsis"
               )}
             >
               {contactItem?.link['text']}
@@ -200,7 +199,13 @@ function Contacts() {
 
 function Socials() {
   return (
-    <ul className="flex gap-4 xl:justify-center">
+    <ul
+      className={clsx(
+        "flex",
+        "gap-4",
+        "xl:justify-center"
+      )}
+    >
       <li>
         <Button
           iconOnly
@@ -209,7 +214,10 @@ function Socials() {
           asChild
           className="text-white"
         >
-          <Link href="https://facebook.com/mahimfarhad" title="facebook">
+          <Link
+            href="https://facebook.com/mahimfarhad"
+            title="facebook"
+          >
             <Icon name="Facebook" />
           </Link>
         </Button>
@@ -223,7 +231,10 @@ function Socials() {
           asChild
           className="text-white"
         >
-          <Link href="https://twitter.com/mahimfarhad" title="twitter">
+          <Link
+            href="https://twitter.com/mahimfarhad"
+            title="twitter"
+          >
             <Icon name="Twitter" />
           </Link>
         </Button>
@@ -237,7 +248,10 @@ function Socials() {
           asChild
           className="text-white"
         >
-          <Link href="https://instagram.com/mahimfarhad" title="instagram">
+          <Link
+            href="https://instagram.com/mahimfarhad"
+            title="instagram"
+          >
             <Icon name="Instagram" />
           </Link>
         </Button>
@@ -251,7 +265,10 @@ function Socials() {
           asChild
           className="text-white"
         >
-          <Link href="https://dribbble.com/mahimfarhad" title="dribbble">
+          <Link
+            href="https://dribbble.com/mahimfarhad"
+            title="dribbble"
+          >
             <Icon name="Dribbble" />
           </Link>
         </Button>
@@ -261,17 +278,15 @@ function Socials() {
 }
 
 function Aside() {
-  const [open, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const windowSize = useWindowSize();
 
-  const animate = {
-    height: windowSize < 1240 ? (open ? "auto" : 0) : "auto",
-    opacity: windowSize < 1240 ? (open ? 1 : 0) : 1,
-    transition: {
-      type: "tween"
-    }
-  };
+  const animate = useMemo(() => ({
+    height: windowSize < 1240 ? (isOpen ? "auto" : 0) : "auto",
+    opacity: windowSize < 1240 ? (isOpen ? 1 : 0) : 1,
+    transition: { type: "tween" }
+  }), [windowSize, isOpen]);
 
   return (
     <LazyMotion
@@ -290,7 +305,7 @@ function Aside() {
           "transition-[var(--transition-1)]"
         )}
       >
-        <Info open={open} setOpen={setOpen} />
+        <Info isOpen={isOpen} setIsOpen={setIsOpen} />
 
         <m.div
           initial={{ height: 0 }}
@@ -303,7 +318,7 @@ function Aside() {
         >
           <Box
             className={clsx(
-              "flex flex-col",
+              "flex flex-col"
             )}
           >
             <Separator
